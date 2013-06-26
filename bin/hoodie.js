bdda27bbf1ccaf5521f3330373b87067a179df66
Sven Lito <me@svenlito.com>
2013-06-26 16:03:16 +0100

    adds 'uninstall' command

--- bin/hoodie.js
+++ bin/hoodie.js
@@ -1,14 +1,6 @@
 #!/usr/bin/env node
 
-/*!
- * Module dependencies.
- */
-
 var CLI = require('../lib/cli'),
     argv = require('optimist').argv;
-
-/*!
- * Run the command-line client.
- */
 
 var cli = new CLI().argv(argv);
