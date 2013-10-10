bc2cbb8497165b108a998b32b51a78d47d2e6ecb
Sven Lito <me@svenlito.com>
2013-10-10 14:55:49 +0100

    [test] adds more tests
    This commit was sponsored by pebble {code}
    http://pebblecode.com

--- spec/hoodie.spec.js
+++ spec/hoodie.spec.js
@@ -8,7 +8,26 @@ var expect = require('expect.js');
  */
 
 describe('hoodie', function() {
+
+  it('should define hoodie.help', function() {
+    expect(hoodie).to.have.property('help');
+  });
+
+  it('should define hoodie.install', function() {
+    expect(hoodie).to.have.property('install');
+  });
+
   it('should define hoodie.new', function() {
     expect(hoodie).to.have.property('new');
   });
+
+  it('should define hoodie.start', function() {
+    expect(hoodie).to.have.property('start');
+  });
+
+  it('should define hoodie.uninstall', function() {
+    expect(hoodie).to.have.property('uninstall');
+  });
+
 });
+
